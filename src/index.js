70aaec569d79f25a7de9ccd366284d2cec903b53
Yauhen Klimovich <webrubin1987@gmail.com>
2023-01-17 09:28:06 +0300

    feat:  brackets task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(stringValue, bracketsConfig) {
+
+    let myArray = ['I', 'Love', 'Rss', 'school'];
+    let newLength = stringValue.length / (myArray.length-2);
+
+    let value = bracketsConfig.map((element) =>
+        element.join(''));
+
+    console.log(value);
+
+    for (let i = myArray.length-4; i < newLength; i++) {
+        value.forEach(element => {
+            stringValue = stringValue.replace(element, '');
+        });
+    }
+	return stringValue.length == myArray.length-4;
 }
